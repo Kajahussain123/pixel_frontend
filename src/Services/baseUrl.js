4e57d99bb7c5e2663e1644bb6d9a25e862dfbb6c
Kajahussain123 <kajahusain513@gmail.com>
2025-03-18 12:21:55 +0530

    updates

--- src/Services/baseUrl.js
+++ src/Services/baseUrl.js
@@ -1,3 +1,3 @@
-// export const BASE_URL = "https://plantmyad.com"
-export const BASE_URL = "http://localhost:3005"
+export const BASE_URL = "https://plantmyad.com"
+// export const BASE_URL = "http://localhost:3005"
 // export const BASE_URL = "https://pixel-backend-83kw.onrender.com"
